1a0a7b0111476cc54f8b356382dbf1eea328aebd
javawithlwl <119342072+javawithlwl@users.noreply.github.com>
2023-06-28 08:35:17 +0530

    Update IplStatsController.java
    fixed complier issue

--- src/main/java/com/careerit/iplstats/controller/IplStatsController.java
+++ src/main/java/com/careerit/iplstats/controller/IplStatsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 @RestController
